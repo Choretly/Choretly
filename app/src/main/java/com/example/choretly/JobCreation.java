3c5f75a11fe9a3b1f89b8a5cfc3c659588caee62
marchetta10 <antoniomarchetta0131321@gmail.com>
2021-03-28 22:43:39 -0400

    Update Job Creation Layout screen

---
+++ app/src/main/java/com/example/choretly/JobCreation.java
@@ -0,0 +1,14 @@
+package com.example.choretly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_creation);
+    }
+}
